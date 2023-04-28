3a1ab534796e4306dd42ac356cce11618fd474b3
prakash472 <prakashparajuli472@gmail.com>
2023-04-28 12:15:38 -0700

    Footer Fixed

--- frontend/src/components/Footer.js
+++ frontend/src/components/Footer.js
@@ -1,6 +1,6 @@
 const footer = () => {
   return (
-    <footer className="bg-gray-800 text-white p-4 mt-10 absolute bottom-0 w-full">
+    <footer className="bg-gray-800 text-white p-4 fixed bottom-0 left-0 right-0">
       <div className="container mx-auto">
         <p className="text-center">
           © 2023 Prakash Parajuli. All rights reserved.
